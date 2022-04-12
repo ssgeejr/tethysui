04a817f58b7de7b3d6a60be006fbe227cb9c6fbd
steven gee <ioexcept@gmail.com>
2022-04-12 14:31:55 -0500

    tethys-2.0 webui is now in a working state. However, the UI needs some beautifucation ...

--- src/main/java/com/eightfourteennorth/tethysui/TethysServlet.java
+++ src/main/java/com/eightfourteennorth/tethysui/TethysServlet.java
@@ -4,16 +4,14 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import java.text.DecimalFormat;
 
 public class TethysServlet extends HttpServlet {
 	private String message;
@@ -22,8 +20,13 @@ public class TethysServlet extends HttpServlet {
       // Do required initialization
       message = "Shamalamadingdong";
 	}
-	
-   public void doGet(HttpServletRequest request, HttpServletResponse response)
+		
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+		  doPost(request,response);
+	  }
+	  
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		Context ctx = null;
@@ -31,24 +34,219 @@ public class TethysServlet extends HttpServlet {
 		Statement stmt = null;
 		ResultSet rs = null;
 		response.setContentType("text/html");
+		out.println("<a href=\"/tethysui/\">[HOME]</a><br>");
+		String alpha = request.getParameter("alpha").trim();
+		String beta = request.getParameter("beta").trim();
+		beta = (beta.length() < 5)?alpha:beta;
+		out.println(alpha + "<br>");
+		out.println(beta + "<br>");
+		
+		//Input Validation
+		if(alpha.length() != 5 || beta.length() != 5) {
+			out.println("Invalid format, must match MMYY[A|B]<br>");
+			return;
+		}
+			
+		PreparedStatement fetchGroupTotals = null;
+		int total = 0;
+		int totalNew = 0;
+		int totalClosed = 0;
+		
+		int[] totalItems = new int[4];
+		int[] newItems = new int[4];
+		int[] closedItems = new int[4];
+	
 		try{
 			ctx = new InitialContext();
 			DataSource ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/scds");
 			con = ds.getConnection();
 			stmt = con.createStatement();
 			
-			rs = stmt.executeQuery("select distinct host as dh, pluginid, name from scorecard limit 10");
+			String queryTotals = "select"
+				+"\n riskid as rid,"
+				+"\n count(distinct hash) as total"
+				+"\n from"
+				+"\n scorecard"
+				+"\n where"
+				+"\n dtkey = ?"
+				+"\n group by"
+				+"\n riskid"
+				+"\n order by"
+				+"\n riskid";
+				
+			fetchGroupTotals = con.prepareStatement(queryTotals);
+			fetchGroupTotals.setString(1,alpha);
+			rs = fetchGroupTotals.executeQuery();
+			out.println("************************************<br>");
+			out.println("<b>Issues</b><br>");
 			while(rs.next()){            
-				out.println(rs.getString("dh") + " * " + rs.getString("pluginid") + " * " + rs.getString("name") + "<br>");
+				total+= rs.getInt("total");
+				totalItems[rs.getInt("rid")] = rs.getInt("total");				
             }
+			out.println("Total: " + total + "<br>");
+			out.println("Critical: " + totalItems[0] + "<br>");
+			out.println("High: " + totalItems[1] + "<br>");
+			out.println("Medium: " + totalItems[2] + "<br>");
+			out.println("Low: " + totalItems[3] + "<br>");
+		
+			out.println("************************************<br>");
+
+			fetchGroupTotals = con.prepareStatement("select "
+				+"\n riskid as rid,"
+				+"\n count(distinct hash) as total"
+				+"\n from"
+				+"\n scorecard"
+				+"\n where"
+				+"\n dtkey = ?"
+				+"\n and riskid = ?"
+				+"\n and hash not in ("
+				+"\n select"
+				+"\n distinct hash"
+				+"\n from "
+				+"\n scorecard"
+				+"\n where"
+				+"\n dtkey = ?"
+				+"\n and riskid = ?"
+				+"\n )"
+				+"\n group by"
+				+"\n rid");
+				
+			fetchGroupTotals.setString(1,alpha);
+			fetchGroupTotals.setString(3,beta);
+			for( int loc = 0; loc<4; loc++){
+				fetchGroupTotals.setInt(2,loc);
+				fetchGroupTotals.setInt(4,loc);
+				rs = fetchGroupTotals.executeQuery();
+				if(rs.next()){ 
+					newItems[loc] = rs.getInt("total");
+					totalNew += rs.getInt("total");
+				}
+			}
+			out.println("************************************<br>");
+			out.println("<b>New Issues</b><br>");
+			out.println("Total New: " + totalNew + "<br>");
+			out.println("Critical New: " + newItems[0] + "<br>");
+			out.println("High New: " + newItems[1] + "<br>");
+			out.println("Medium New: " + newItems[2] + "<br>");
+			out.println("Low New: " + newItems[3] + "<br>");
+			out.println("************************************<br>");
+			
+			
+			fetchGroupTotals.setString(3,alpha);
+			fetchGroupTotals.setString(1,beta);
+			for( int loc = 0; loc<4; loc++){
+				fetchGroupTotals.setInt(2,loc);
+				fetchGroupTotals.setInt(4,loc);
+				rs = fetchGroupTotals.executeQuery();
+				if(rs.next()){ 
+					closedItems[loc] = rs.getInt("total");
+					totalClosed += rs.getInt("total");
+				}
+			}
+			out.println("************************************<br>");
+			out.println("<b>Closed Issues</b><br>");
+			out.println("Total New: " + totalClosed + "<br>");
+			out.println("Critical New: " + closedItems[0] + "<br>");
+			out.println("High New: " + closedItems[1] + "<br>");
+			out.println("Medium New: " + closedItems[2] + "<br>");
+			out.println("Low New: " + closedItems[3] + "<br>");
+			out.println("************************************<br>");
+
+
+			fetchGroupTotals = con.prepareStatement("select "
+				+"\n count(distinct hash) as total,"
+				+"\n pluginid,"
+				+"\n name  "
+				+"\n from"
+				+"\n scorecard"
+				+"\n where"
+				+"\n dtkey = ?"
+				+"\n and riskid = ?"
+				+"\n group by"
+				+"\n name, pluginid"
+				+"\n order by"
+				+"\n total desc");
+				
+				
+				
+			String[] riskv = {"Critical", "High", "Medium", "Low"};
+			fetchGroupTotals.setString(1,alpha);
+			out.println("************************************<br>");
+			out.println("************<b>TOP 10 RISKS</b>*************<br>");
+			
+			DecimalFormat df = new DecimalFormat();
+			df.setMaximumFractionDigits(2);
+//			System.out.println() );
+			
+			for(int risks = 0; risks < 4; risks++){
+				out.println("************************************<br>");
+				out.println("****" + riskv[risks] + "****<br>");
+				fetchGroupTotals.setInt(2,risks);
+				rs = fetchGroupTotals.executeQuery();
+				float denom = totalItems[risks];
+				float numer = 0f;
+				for(int top = 0; top < 10; top++){
+					rs.next();
+					numer = rs.getFloat("total");
+					out.println(df.format( ((numer/denom)*100))  + "% * " + rs.getInt("total") + " * " + rs.getString("pluginid") + " * " + rs.getString("name") + "<br>");
+				}
+				out.println("************************************<br>");
+			}
+			
+				
+				
+/*			
+			T
+			N
+			C
+			Critical
+			T
+			N
+			C
+			High
+			T
+			N
+			C
+			Medium
+			T
+			N
+			C
+			Low
+			T
+			N
+			C
+*/			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 		}catch(NamingException e){
 			e.printStackTrace();
 		} catch (SQLException e) {
+			System.out.println("XXXXX--------------------------------------------XXXXX");
 			e.printStackTrace();
+			System.out.println("XXXXX--------------------------------------------XXXXX");
 		}finally{
 			try {
 				rs.close();
 				stmt.close();
+				fetchGroupTotals.close();
 				con.close();
 				ctx.close();
 			} catch (SQLException e) {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,83 @@
-<html>
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="en-us" http-equiv="Content-Language" />
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Tethys Web Interface</title>
+
+	<!-- <link rel="stylesheet" href="mystyle.css"> -->
+	<style>
+	.query {
+	  margin-left: auto;
+	  margin-right: auto;
+	  width: 800px;
+	}
+	
+	.auto-style1 {
+		text-align: right;
+	}
+	
+	.auto-style2 {
+		text-align: center;
+	}
+	
+	</style>
+	
+	<script>
+	
+	function validate(){
+		regex = new RegExp('^([0-9]{4})(A|B)$');
+		var alpha = document.getElementById('alpha').value
+		var beta = document.getElementById('beta').value
+		if(!regex.test(alpha)){
+			alert('Invalid Alpha Value, MMYY[A|B]')
+			return false;
+		}
+		if(!regex.test(beta)){
+			alert('Invalid Beta Value, MMYY[A|B]')
+			return false;
+		}
+		
+		return true;
+	}
+	
+	</script>
+
+</head>
+
 <body>
-<h2>Hello World!</h2>
+
+
+<form action="/tethysui/query" id="query" method="post" onsubmit="return validate()">
+
+<table class="query">
+	<tr>
+		<td class="auto-style2">Tethys Web Interface</td>
+	</tr>
+	<tr>
+		<td>
+		<table style="width: 100%">
+			<tr>
+				<td class="auto-style1">Alpha</td>
+				<td><input name="alpha" id="alpha" type="text" size="6" />&nbsp;</td>
+				<td class="auto-style1">Beta</td>
+				<td><input name="beta" id="beta" type="text" size="6" /></td>
+				<td class="auto-style1">
+				<input name="qb" type="submit" value="Query" />&nbsp;</td>
+			</tr>
+		</table>
+		</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+	</tr>
+</table>
+
+
+</form>
+
+
 </body>
+
 </html>
